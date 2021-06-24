4d04abc19ce8c56330b632e03798f65d6dfae886
vagrant <vagrant@x.dev>
2021-06-24 08:49:05 +0000

    g scaffold meetings

--- app/controllers/meeting_controller.rb
+++
@@ -1,6 +0,0 @@
-class MeetingController < ApplicationController
-  def index
-    @partners = User.all.select { |u| u.is_partner }
-    @entrepreneur = User.all.select { |u| u.is_entrepreneur }
-  end
-end
